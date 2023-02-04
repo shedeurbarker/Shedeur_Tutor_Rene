59d9449edeca5c924b70880699845bd0684e45f3
shedeurapps <abarker@st.ug.edu.gh>
2023-02-04 13:56:24 +0000

    added if-else conditions to Calculator Class

--- CalculatorApp/src/Calculator.java
+++ CalculatorApp/src/Calculator.java
@@ -2,19 +2,29 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class Calculator {
+    static int action_today;
     public static void main (String[] arg) {
 
         Area area = new Area();
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter Base: ");
-        area.base = scan.nextDouble();
+        System.out.println("What do you want to do today:");
+        System.out.println("Enter 1 - Areas of Triangles");
+        System.out.println("Enter 2 - Areas of Rectangle");
+        System.out.println("Enter 3 - Areas of Circles");
+        System.out.println("Enter 4 - For General Arithmetic");
+        System.out.print("Your Choice: ");
+        action_today = scan.nextInt();
 
-        System.out.print("Enter Height: ");
-        area.height = scan.nextDouble();
+        if(action_today == 1) { // Triangles
+            System.out.print("Enter Base: ");
+            area.base = scan.nextDouble();
+            System.out.print("Enter Height: ");
+            area.height = scan.nextDouble();
+            System.out.println("The Area of your rectangle is: " + area.Triangle());
+        }
 
-        System.out.println(area.Triangle());
 
 
 
